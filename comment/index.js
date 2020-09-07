626d3560738b1d51084b30edd075aef5b51b778c
David Yim <yimd85@gmail.com>
2020-09-07 17:28:47 -0400

    updated comment

--- comment/index.js
+++ comment/index.js
@@ -1,22 +1,29 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
 
 const app = express();
+app.use(bodyParser.json());
 
-const comments = {};
+const commentsByPostId = {};
 
-app.get('/comments', (req, res) => {
-
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
+app.get("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
 
+  const comments = commentsByPostId[req.params.id] || [];
 
-app.post('/comments', (req, res) => {
+  comments.push({ id: commentId, content });
 
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
-
-
 
 app.listen(4001, () => {
-    console.log('Listening on 4001')
+  console.log("Listening on 4001");
 });
